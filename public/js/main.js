34776b3c48c41b58154b513c7d532da05bdff3c2
t0md3v <tom.infotek@gmail.com>
2022-09-11 14:00:00 +0101

    more edits

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,7 @@
 const deleteBtn = document.querySelectorAll('.del')
 const todoItem = document.querySelectorAll('span.not')
 const todoComplete = document.querySelectorAll('span.completed')
+const todoEdit = document.querySelectorAll('span.edit')
 
 Array.from(deleteBtn).forEach((el)=>{
     el.addEventListener('click', deleteTodo)
@@ -13,6 +14,7 @@ Array.from(todoItem).forEach((el)=>{
 Array.from(todoComplete).forEach((el)=>{
     el.addEventListener('click', markIncomplete)
 })
+Array.from(todoEdit).forEach((el)=>{el.addEventListener('click',editTodo)})
 
 async function deleteTodo(){
     const todoId = this.parentNode.dataset.id
@@ -51,6 +53,24 @@ async function markComplete(){
 }
 
 async function markIncomplete(){
+    const todoId = this.parentNode.dataset.id
+    try{
+        const response = await fetch('todos/markIncomplete', {
+            method: 'put',
+            headers: {'Content-type': 'application/json'},
+            body: JSON.stringify({
+                'todoIdFromJSFile': todoId
+            })
+        })
+        const data = await response.json()
+        console.log(data)
+        location.reload()
+    }catch(err){
+        console.log(err)
+    }
+}
+
+async function editTodo() {
     const todoId = this.parentNode.dataset.id
     try{
         const response = await fetch('todos/markIncomplete', {
